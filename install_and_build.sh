3c5e68892ccd8ca37493afeabb72d6c85d780745
Noah0615 <101160620+Noah0615@users.noreply.github.com>
2025-09-30 21:19:32 +0900

    Add files via upload

---
+++ install_and_build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo 'This is an installation and build script for beautiAI.'
+
+echo '1. Installation'
+echo 'Installing dependencies from requirements.txt...'
+pip install -r requirements.txt
+
+echo 'Installation complete.'
+
+echo '2. Run'
+echo 'To run the application, execute the following command:'
+echo 'python app.py'
